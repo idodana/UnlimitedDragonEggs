e49cc8e87875014ab6d99c6606d54bb70f219e0f
idodana <idodana01@gmail.com>
2020-07-22 17:00:18 +0300

    Update fabric.mod.json

--- src/main/java/ido/unlimiteddragoneggs/UnlimitedDragonEggs.java
+++
@@ -1,9 +0,0 @@
-package ido.unlimiteddragoneggs;
-
-import net.fabricmc.api.ModInitializer;
-
-public class UnlimitedDragonEggs implements ModInitializer {
-	@Override
-	public void onInitialize() {
-	}
-}
