fd98ed44d2a6cf4484573c93ee1958ead466b0ca
idodana <idodana01@gmail.com>
2020-07-22 10:07:04 +0300

    Update UnlimitedDragonEggs.java

--- src/main/java/ido/unlimiteddragoneggs/UnlimitedDragonEggs.java
+++ src/main/java/ido/unlimiteddragoneggs/UnlimitedDragonEggs.java
@@ -3,7 +3,6 @@ package ido.unlimiteddragoneggs;
 import net.fabricmc.api.ModInitializer;
 
 public class UnlimitedDragonEggs implements ModInitializer {
-	public static final String MODID = "unlimiteddragoneggs";
 	@Override
 	public void onInitialize() {
 	}
